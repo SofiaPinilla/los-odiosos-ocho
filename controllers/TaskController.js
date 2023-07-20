036c1aa90f7b4e748c0edf184a31ca587b9357ee
pafz <paferza@gmail.com>
2023-07-20 11:26:42 +0200

    getById

--- controllers/TaskController.js
+++ controllers/TaskController.js
@@ -1,26 +1,34 @@
-const Task = require("../models/Task")
+const Task = require('../models/Task');
 
 const TaskController = {
-    async create (req, res) {
-        try {
-            req.body.completed = false
-            const task = await Task.create(req.body)
-            res.status(201).send({msg: "Tarea creada con exito", task})
-        } catch (error) {
-            console.error(error)
-            res.status(500).send({msg:"Error al crear la tarea", error})
-        }
-    },
-    async getAll(req, res) {
-        try {
-            const tasks = await Task.find()
-            res.status(200).send({msg: "Aqui tienes todas las tareas", tasks})
-        } catch (error) {
-            console.error(error)
-            res.status(500).send({msg: "Error al traer todas las tareas", error})
-        }
+  async create(req, res) {
+    try {
+      req.body.completed = false;
+      const task = await Task.create(req.body);
+      res.status(201).send({ msg: 'Tarea creada con exito', task });
+    } catch (error) {
+      console.error(error);
+      res.status(500).send({ msg: 'Error al crear la tarea', error });
     }
-
-}
+  },
+  async getAll(req, res) {
+    try {
+      const tasks = await Task.find();
+      res.status(200).send({ msg: 'Aqui tienes todas las tareas', tasks });
+    } catch (error) {
+      console.error(error);
+      res.status(500).send({ msg: 'Error al traer todas las tareas', error });
+    }
+  },
+  async getTaskById(req, res) {
+    try {
+      const taskById = await Task.findById(req.params._id);
+      res.status(200).send({ msg: 'Aquí tienes la tarea por el id', taskById });
+    } catch (error) {
+      console.error(error);
+      res.status(500).send({ msg: 'Error al traer la tarea', error });
+    }
+  },
+};
 
 module.exports = TaskController;
--- routes/tasks.js
+++ routes/tasks.js
@@ -1,8 +1,9 @@
-const express = require("express")
-const TaskController = require("../controllers/TaskController")
-const router = express.Router()
+const express = require('express');
+const TaskController = require('../controllers/TaskController');
+const router = express.Router();
 
-router.post("/", TaskController.create)
-router.get("/get",TaskController.getAll)
+router.post('/', TaskController.create);
+router.get('/get', TaskController.getAll);
+router.get('/getTaskById/:_id', TaskController.getTaskById);
 
-module.exports = router
+module.exports = router;
